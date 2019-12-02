140efd5ee77172e52e99b173b68c6f66e0e6d603
saiful8198 <saiful30412@gmail.com>
2019-12-02 12:06:15 +0700

    Initial commit

---
+++ app/src/main/java/com/example/faiz/Main2Activity.java
@@ -0,0 +1,29 @@
+package com.example.faiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+
+
+public class Main2Activity extends AppCompatActivity {
+    Button button2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        button2 = findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent a = new Intent(Main2Activity.this, Main3Activity.class);
+                Main2Activity.this.startActivity(a);
+                Main2Activity.this.finish();
+            }
+    });
+    }
+}
---
+++ app/src/main/java/com/example/faiz/Main3Activity.java
@@ -0,0 +1,27 @@
+package com.example.faiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+
+public class Main3Activity extends AppCompatActivity {
+    Button button3;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+        button3 = findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent a = new Intent(Main3Activity.this, MainActivity.class);
+                Main3Activity.this.startActivity(a);
+                Main3Activity.this.finish();
+            }
+        });
+    }
+}
